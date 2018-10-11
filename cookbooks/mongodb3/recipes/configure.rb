3d10badcde7e13d060f3312f229815b0e03c0d0f
Braulio Padilla <braulio@nclouds.com>
2018-10-11 13:10:32 -0600

    Updating recipes and cloudofrmation stack

--- cookbooks/mongodb3/recipes/configure.rb
+++ cookbooks/mongodb3/recipes/configure.rb
@@ -1,6 +1,10 @@
+package 'awscli'
 # Configure replicas
-this_instance = search("aws_opsworks_instance", "self:true").first
-layer_id      = this_instance["layer_ids"][0]
+this_instance             = search("aws_opsworks_instance", "self:true").first
+layer_id                  = this_instance["layer_ids"][0]
+# availability_zone         = this_instance["availability_zone"]
+# n = availability_zone.size
+# region=availability_zone[0..n-2]
 mongo_nodes = []
 search("aws_opsworks_instance", "layer_ids:#{layer_id}").each do |instance|
   mongo_nodes.push(instance['hostname'])
@@ -9,13 +13,15 @@ end
 # TODO Get the node status, and exist until they all are in online state
 ruby_block 'configure_replicas' do
   block do
-    if this_instance["hostname"] == "#{node['master_node']}"
-      sleep(180)
+    if "#{node['is_initiated']}" == "no"
+      Chef::Log.info "Initializing replica set"
       system("echo \"rs.initiate()\" | mongo")
-      mongo_nodes.each do |host|
-        if host != this_instance["hostname"]
-          system("echo \"rs.add('#{host}:27017')\" | mongo")
-        end
+      system("aws opsworks --region us-east-1 update-layer --layer-id #{layer_id} --custom-json " + '"{\"is_initiated\":\"yes\"}"' )
+    end
+    mongo_nodes.each do |host|
+      Chef::Log.info "Adding nodes"
+      if host != this_instance["hostname"]
+        system("echo \"rs.add('#{host}:27017')\" | mongo")
       end
     end
   end
